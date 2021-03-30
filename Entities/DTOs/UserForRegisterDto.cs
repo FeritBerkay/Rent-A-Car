c205c8e64c84776544a27d0296af76ffdbdddc83
FeritBerkay <76204009+FeritBerkay@users.noreply.github.com>
2021-03-30 09:42:43 +0300

    Add files via upload

---
+++ Entities/DTOs/UserForRegisterDto.cs
@@ -0,0 +1,10 @@
+﻿using Core.Entities;
+
+public class UserForRegisterDto : IDto
+{
+    //Dto cunku datamda password yok. Ve register bilgilerini degistirebilirim.
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
